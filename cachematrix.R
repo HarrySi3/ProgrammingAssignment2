d393075e5dc20a64e08488f67df1f60d419c6295
HarrySi3 <harrys@sprynet.com>
2015-07-26 11:52:37 -0700

    Assignment2 updates
    This change contains work for creating functions for Assignment 2 and
    commenting.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that is a contains a sets and gets the value of matrix object that 
+## is stored in a cache in a separate environment
+##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## CacheSolve is a function that computes the inverse of the matrix originally created by 
+## makeCacheMatrix above. The function will determine if the inverse has already been 
+## calculated and if this resolves as true, it will then retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse() ## This function attempts to get the matrix object
+  if (!is.null(m)) { ## This function tests to see if the cached data is null.
+    message("getting cached data") ## This prints to the console a message if the cached data was found. If found it will retrieve the data from the cache.
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat, ...) ## This function attempts to solves the equation for containing the matrix
+  x$setInverse(m) 
+  m
 }
