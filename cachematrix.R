5370fe8fca7b2be5aab4c377e6acf8f1f315fb96
HarrySi3 <harrys@sprynet.com>
2015-09-26 23:35:42 -0700

    Revert "Comments for Assignment2"
    This reverts commit 8b0162bc2903e899972e4d100e31b235f92de5a1.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,6 @@
 ## makeCacheMatrix is a function that is a contains a sets and gets the value of matrix object that 
 ## is stored in a cache in a separate environment
 ##
-## Tested using the following matrix: 
-## mb <-makeCacheMatrix(matrix(c(1,2,2,1),nrow=2,ncol=2)) 
-## cacheSolve(mb)
 
 makeCacheMatrix <- function(x = matrix()) { 
   m <- NULL
@@ -34,4 +31,3 @@ cacheSolve <- function(x, ...) {
   x$setInverse(m) 
   m
 }
-
